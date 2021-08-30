9d1e9dfd10bda95f1e7941a67f395819cceb226e
Saeed Malami <saeed@openmindplatform.org>
2021-08-30 00:41:46 -0400

    Adds Participant Progress Table functions and styling

--- Blank Page Templates/page-template-blank.php
+++ Blank Page Templates/page-template-blank.php
@@ -8,13 +8,14 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() ); ?>
 	<head>
 		<title>OpenMind</title>
 		<link rel="icon" href="https://openmindplatform.org/wp-content/uploads/2017/11/cropped-OM-Icon-32x32.png" sizes="32x32" />
-		
+
 		<meta content="width=device-width, initial-scale=1.0" name="viewport">
 		<script src="https://www.guidedtrack.com/assets/jquery_gt.js"></script>
 		<script src="https://www.guidedtrack.com/assets/interpreter.js?v=1"></script>
 		<link rel="stylesheet" type="text/css" href="https://www.guidedtrack.com/assets/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="https://1061174115.rsc.cdn77.org/css_samples/guidedtrack_legacy.css">
+		 <link rel="stylesheet" type="text/css" href="https://1061174115.rsc.cdn77.org/css_samples/guidedtrack_legacy.css">
 		<link href="https://fonts.googleapis.com/css?family=Arimo:400,400i,700,700i|PT+Serif:400,400i,700,700i" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 		<style>
 			body {
@@ -856,6 +857,77 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() ); ?>
 					font-size: 180%;
 				}
 			}
+
+			/* Styling for Participant Progress Table div */
+			.table-button{ 
+			    display: inline-block;
+			    border-width: 2px;
+			    border-radius: 3px;
+			  }
+			#s-btn {
+			  margin-left: -6px;
+			}
+			#pp-display {
+			    margin-top: 15px;
+			    font-size: 12px;
+			  }
+
+			#dl-button, #searchBox {
+			    float: right;
+			    margin-right: 5px;
+			  }
+
+			.styled-table{ overflow:auto; }
+
+
+			.styled-table table{
+			    width: 100%;
+			    table-layout: auto;
+			    margin: auto;
+			    margin-top: 15px;
+			    border-collapse: collapse;
+			    font-size: 14px;
+			    font-family: sans-serif;
+			    border-spacing: 0;
+			    overflow: hidden;
+			    margin-bottom: 0px;
+			}
+
+
+			.styled-table thead tr {
+			    white-space: nowrap;
+			    background-color: #00B496;
+			    color: #ffffff;
+			    text-align: left;
+			    font-size: 14px;
+			    cursor:  pointer;
+			}
+
+			.styled-table thead th{ 
+			   padding: 12px 12px 12px 8px;
+			}
+
+
+
+			.styled-table th,
+			.styled-table td {
+			    padding: 5px 8px;
+			}
+
+
+
+			.styled-table tbody tr {
+			    border-bottom: 1px solid #dddddd;
+
+			}
+
+			.styled-table tbody tr:nth-of-type(even) {
+			    background-color: #f3f3f3;
+			}
+
+			.styled-table tbody tr:last-of-type {
+			    border-bottom: 2px solid #00B496;
+			}
 		</style>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177276190-1"></script>
@@ -866,10 +938,247 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() ); ?>
 
   gtag('config', 'UA-177276190-1');
 </script>
-	<?php wp_head(); ?>
+
 	</head>
 	
     <script>
+
+    	//Sets default values
+		var AccessCode, csvHeaders, scoreHeaders, progressHeaders, tableData={};
+
+		$(window).on("loadData", function(event, data){
+		    tableData = data;
+		});
+
+		//Loads CSV table headers and initializes table data object
+		$(window).on("loadSettings", function(event, data){
+		  tableData={};
+		  AccessCode = data.AccessCode;
+		  csvHeaders = data.csvHeaders; 
+		     
+		});
+
+		//Loads Headers and calls function to generate table 
+		$(window).on("loadHeaders", function(event, data){
+
+		  // Sets score and progress table headers expected by AccessCode
+		  scoreHeaders = data.score;
+		  progressHeaders = data.progress;
+
+		  // Displays participant progress table
+		  $(".main").before($('#pp-display'));
+		  $('.main').css("padding-top","0em");
+
+		  $('#pp-display').css("display","block");
+		  
+
+		  // Increases size of container to hold table
+		  $(".container").css("width","1400px");
+
+		  //Defaults to generate progress table 
+		  btnSelected(1);
+		  generateTable(progressHeaders, tableData);
+
+		});
+
+		// Closes table and removes div visibility
+		$(window).on("closeTable", function(event, data){
+		   tableData = {};
+		  // Decreases size of container to default
+		    $(".container").css("width","1170px");
+		    $('.main').css("padding-top","3em");
+		  // Hides participant progress table 
+		  $('#pp-display').css("display","none");
+
+		});
+
+		// Adds individual users to tableData object with OMID
+		$(window).on("addUser", function(event, data){
+		    tableData[data.OMID] = {};
+		    tableData[data.OMID] = data;
+		    delete tableData[data.OMID]['OMID'];
+		});
+
+		// Uses OMID to add Participant progress data to tableData object
+		$(window).on("addUserData", function(event, data){
+		    try{
+			    Object.assign(tableData[data.OMID], data);
+			    delete tableData[data.OMID]['OMID'];
+		    }
+		    catch(err){
+		     	console.log(err);
+		    }
+		});
+
+		function generateTable(headers, data) {
+		  //Creates table
+		  var table = document.createElement("table");
+		  table.id='pp-table';
+		  //Inserts empty header row at top
+		  var header = table.createTHead();
+		  // Create an empty <tr> element and add it to the first position of <thead>:
+		  var tr = header.insertRow(0);    
+
+		  for (var i = 0; i < headers.length; i++) {
+		    var th = document.createElement("th");      // table header.
+		    th.setAttribute("onclick", "sortTable("+i+")" ); //makes headers clickable for sorting
+		    th.innerHTML = headers[i] + " ";
+		    th.innerHTML += '<i class="fa fa-sort" aria-hidden="true"></i>';
+		    tr.appendChild(th);
+		  }
+		  var row, body = table.createTBody();
+		  // Adds data to the table mapped as rows.
+		  for (var item in data) {
+		    let record = data[item];
+		    row = body.insertRow(-1);
+		    for (var j = 0; j < headers.length; j++) {
+		      var tabCell = row.insertCell(-1);
+		      if (record[headers[j]] != null)
+		        tabCell.innerHTML = record[headers[j]];
+		    }
+		  }
+
+		            var divShowData = document.getElementById('showData');
+		            divShowData.innerHTML="";
+		  divShowData.appendChild(table);
+		  
+		} 
+
+		// Sets color selected for table buttons 
+		function btnSelected(n){
+			 if(n==1){ 
+			   $('#p-btn').css('background','#00B496');
+			   $('#p-btn').css('color','white');
+			   $('#s-btn').css('background','#fff');
+			   $('#s-btn').css('color', '#333');
+			   
+			 }
+			 else {
+			   $('#p-btn').css('background','#fff');
+			   $('#p-btn').css('color', '#333');
+			   $('#s-btn').css('background','#00B496');
+			   $('#s-btn').css('color','white');
+			}
+		}
+
+		//Sorts table using headers
+		function sortTable(n) {
+		  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+		  table = document.getElementById("pp-table");
+		  switching = true;
+		  // Set the sorting direction to ascending:
+		  dir = "asc";
+		  /* Make a loop that will continue until
+		  no switching has been done: */
+		  while (switching) {
+		    // Start by saying: no switching is done:
+		    switching = false;
+		    rows = table.rows;
+		   
+		    for (i = 1; i < (rows.length - 1); i++) {
+		      // Start by saying there should be no switching:
+		      shouldSwitch = false;
+		      /* Get the two elements you want to compare,
+		      one from current row and one from the next: */
+		      x = rows[i].getElementsByTagName("TD")[n];
+		      y = rows[i + 1].getElementsByTagName("TD")[n];
+		      /* Check if the two rows should switch place,
+		      based on the direction, asc or desc: */
+		      if (dir == "asc") {
+		        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+		          // If so, mark as a switch and break the loop:
+		          shouldSwitch = true;
+		          break;
+		        }
+		      } else if (dir == "desc") {
+		        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+		          // If so, mark as a switch and break the loop:
+		          shouldSwitch = true;
+		          break;
+		        }
+		      }
+		    }
+		    if (shouldSwitch) {
+		      /* If a switch has been marked, make the switch
+		      and mark that a switch has been done: */
+		      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+		      switching = true;
+		      // Each time a switch is done, increase this count by 1:
+		      switchcount ++;
+		    } else {
+		      /* If no switching has been done AND the direction is "asc",
+		      set the direction to "desc" and run the while loop again. */
+		      if (switchcount == 0){ 
+		       if(dir == "asc") {
+		        dir = "desc";
+		        switching = true;
+		       }
+		      }
+		    }
+		  }
+		}
+		  
+		//Searches table using search box input
+		function filterList() {
+		  // Declare variables
+		  var input, filter, table, tr, fName, lName, i, txtValue;
+		  input = document.getElementById("searchBox");
+		  filter = input.value.toUpperCase();
+		  table = document.getElementById("pp-table");
+		  tr = table.getElementsByTagName("tr");
+
+		  // Loop through all table rows, and hide those who don't match the search query
+		  for (i = 0; i < tr.length; i++) {
+		    lName = tr[i].getElementsByTagName("td")[0];
+		    fName = tr[i].getElementsByTagName("td")[1];
+		    if (fName || lName)  {
+		      txtValue = lName.textContent || lName.innerText;
+		      txtValue2 = fName.textContent || fName.innerText;
+		      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+		        tr[i].style.display = "";
+		      } 
+		      else if (txtValue2.toUpperCase().indexOf(filter) > -1) {
+		        tr[i].style.display = "";
+		    }
+		      else {
+		        tr[i].style.display = "none";
+		      }
+		    }
+		  }
+		}
+
+		//Triggers CSV download using csv headers
+		function downloadCSV() {
+
+		  var csvTable=[]
+		  
+		    for (var obj in tableData ){
+		      let s = tableData[obj];
+		      csvTable.push(s);
+		    }
+		    const content = csvTable.map(r => Object.values(r).join());
+		    var csv = [csvHeaders].concat(content).join("\n");
+
+		    var exportedFilename = AccessCode + '.csv' || 'export.csv';
+
+		  var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+		  if (navigator.msSaveBlob) { // IE 10+
+		      navigator.msSaveBlob(blob, exportedFilename);
+		  } else {
+		      var link = document.createElement("a");
+		      if (link.download !== undefined) { // feature detection
+		          // Browsers that support HTML5 download attribute
+		            var url = URL.createObjectURL(blob);
+		            link.setAttribute("href", url);
+		            link.setAttribute("download", exportedFilename);
+		            link.style.visibility = 'hidden';
+		            document.body.appendChild(link);
+		            link.click();
+		            document.body.removeChild(link);
+		      }
+		  }
+		}
+
         $(window).on("makeTable", makeTable); 
 
         function makeTable(event, object) {            
@@ -910,19 +1219,34 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() ); ?>
         };
     </script>
 	
-	<body class="openMind-app">
-		<div id="main-content">
+<body class="openMind-app">
+    <div id="pp-display" style="display:none">
+        <br></br>
+        <h3 style="margin-bottom:3px">Participant Progress Dashboard</h3>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<div class="entry-content">
-						<?php
-							the_content();
-						?>
-					</div> <!-- .entry-content -->
-				</article> <!-- .et_pb_post -->
-			<?php endwhile; ?>
+        <p style="font-size:15px">Below you can view the progress of your participants on the OpenMind program. Only participants who have completed at least lesson 1 will display.</p>
 
-		</div> <!-- #main-content -->
-	</body>
+        <input type="text" id="searchBox" onkeyup="filterList()" placeholder="Search for names...">
+
+        <button type="button" onclick="downloadCSV()" id="dl-button">Download</button>
+
+        <button type="button" onclick="generateTable(progressHeaders, tableData);btnSelected(1)" id="p-btn" class="table-button">View Progress</button>
+        <button type="button" onclick="generateTable(scoreHeaders, tableData);btnSelected(2)" id="s-btn" class="table-button">View Scores</button>
+
+    <div class='styled-table' id='showData'></div>
+  </div>
+    <div id="main-content">
+
+      <?php while ( have_posts() ) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <div class="entry-content">
+            <?php
+              the_content();
+            ?>
+          </div> <!-- .entry-content -->
+        </article> <!-- .et_pb_post -->
+      <?php endwhile; ?>
+
+    </div> <!-- #main-content -->
+  </body>
 </html>
